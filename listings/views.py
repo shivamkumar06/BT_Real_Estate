c5751c182d6264e05a6c49cc9b5f2ca4f04f92b7
shivamkumar06 <shivam.saha06@gmail.com>
2021-05-14 00:28:44 +0530

    Listing Dynamic data done

--- listings/views.py
+++ listings/views.py
@@ -1,21 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
 
 # Create your views here.
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings,6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
 
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     return render(request,'listings/listings.html',context)
 
 
 def listing(request,listing_id):
-    return render(request,'listings/listing.html')
+    listing = get_object_or_404(Listing,pk=listing_id)
+
+
+    context = {
+        'listing' : listing
+    }
+
+    return render(request,'listings/listing.html',context)
 
 
 def search(request):
